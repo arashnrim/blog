d0db763073c4c7485d7c989c20d6bdf134d7d350
Arash <47273556+arashnrim@users.noreply.github.com>
2024-05-16 10:26:58 +0800

    feat: fill site with content

---
+++ src/content/config.ts
@@ -0,0 +1,26 @@
+import { defineCollection, reference, z } from "astro:content";
+
+const posts = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishedDate: z.date(),
+    updatedDate: z.date().optional(),
+    topics: z.array(reference("topics")).optional(),
+    image: z.string().optional(),
+  }),
+});
+
+const topics = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+    description: z.string(),
+  }),
+});
+
+export const collections = {
+  posts,
+  topics,
+};
