13190b2ad6912c711f7a1386f5e43f813cd5423b
Arash <47273556+arashnrim@users.noreply.github.com>
2024-05-22 15:10:28 +0800

    feat: improve stylings and import old components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import codeTheme from "./config/code-theme.json";
+import remarkToc from "remark-toc";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), icon()]
+  integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: codeTheme,
+      },
+      remarkPlugins: [remarkToc],
+    }),
+    icon(),
+  ],
 });
