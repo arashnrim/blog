a2bf3c5844155d2c787be4c2727c5ccd5787c0d9
Arash <47273556+arashnrim@users.noreply.github.com>
2024-05-26 11:48:04 +0800

    feat: enable SSR for 404 page
    This makes sure that the error messages are dynamic and that routing works properly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,19 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import codeTheme from "./config/code-theme.json";
 import remarkToc from "remark-toc";
-
 import icon from "astro-icon";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: {
-        theme: codeTheme,
-      },
-      remarkPlugins: [remarkToc],
-    }),
-    icon(),
-  ],
+  output: "hybrid",
+  integrations: [mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: codeTheme
+    },
+    remarkPlugins: [remarkToc]
+  }), icon()],
+  adapter: vercel()
 });
