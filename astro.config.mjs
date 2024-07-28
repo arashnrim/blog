393eb94a260024a767395fe65192ea861c77175e
Arash <47273556+arashnrim@users.noreply.github.com>
2024-07-28 23:46:56 +0800

    feat: set up sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,24 @@ import mdx from "@astrojs/mdx";
 import codeTheme from "./config/code-theme.json";
 import remarkToc from "remark-toc";
 import icon from "astro-icon";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [mdx({
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: codeTheme
-    },
-    remarkPlugins: [remarkToc]
-  }), icon()],
-  adapter: vercel()
+  site: "https://blog.arash.codes",
+  integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: codeTheme,
+      },
+      remarkPlugins: [remarkToc],
+    }),
+    icon(),
+    sitemap(),
+  ],
+  adapter: vercel(),
 });
